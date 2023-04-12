b038ee076080e52d75cb8dd3e263ba764c36931b
Margarita <podezzhikh95@bk.ru>
2023-04-13 00:26:15 +0300

    Описание классов

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,21 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Autor(models.Model):
-    pass
+    authorUser = models.OneToOneField(User, on_delete=models.CASCADE)
+    ratingAuthor = models.SmallIntegerField(default=0)
+
+    def update_rating(self):
+        pass
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=64, unique=True)
 
 
 class Post(models.Model):
-    pass
+    author = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    NEWS = 'NW'
+    ARTICLE = 'AR'
+    CATEGORY_CHOICES = (
+        (NEWS, 'Новость'),
+        (ARTICLE, 'Статья'),
+    )
+    categoryType = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default=ARTICLE)
+    dateCreation = models.DateTimeField(auto_now_add=True)
+    postCategory = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    rating = models.SmallIntegerField(default=0)
+
+    def like(self):
+        pass
+
+    def dislike(self):
+        pass
 
 
 class PostCategory(models.Model):
-    pass
+    postThrough = models.ForeignKey(Post, on_delete=models.CASCADE)
+    categoryThrough = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
-    pass
+    commentPost = models.ForeignKey(Post, on_delete=models.CASCADE)
+    commentUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    dateCreation = models.DateTimeField(auto_now_add=True)
+    rating = models.SmallIntegerField(default=0)
+
+    def like(self):
+        pass
+
+    def dislike(self):
+        pass
