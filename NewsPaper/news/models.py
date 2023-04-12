c9783e5b6d771fbcc876ce1f902cd608892c2831
Margarita <podezzhikh95@bk.ru>
2023-04-12 23:58:44 +0300

    Добавление классов

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Autor(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Post(models.Model):
+    pass
+
+
+class PostCategory(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
