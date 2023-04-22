b28ed29c578433fe17635cff6ed06260313483bf
Margarita <podezzhikh95@bk.ru>
2023-04-23 02:10:08 +0300

    Настройка проекта

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'fpages',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
