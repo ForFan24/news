b0552cdc3dbaab4370213ae5d80c93bb721efec9
Margarita <podezzhikh95@bk.ru>
2023-04-24 23:38:27 +0300

    Настройка вывода новостей

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,12 +1,19 @@
+from datetime import datetime
 from django.views.generic import ListView, DetailView
 from .models import Post
 
 
 class PostList(ListView):
     model = Post
-    ordering = 'title'
+    ordering = '-dateCreation'
     template_name = 'news.html'
     context_object_name = 'news'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = None
+        return context
 
 
 class PostDetail(DetailView):
