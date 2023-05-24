5aedb1866738798f53aaca2f26b2dc975798ba21
Margarita <podezzhikh95@bk.ru>
2023-05-24 23:29:39 +0300

    почтовая рассылка по дате и времени

--- NewsPaper/NewsPaper/management/commands/runapscheduler.py
+++ NewsPaper/NewsPaper/management/commands/runapscheduler.py
@@ -4,14 +4,14 @@ import logging
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
 from django.conf import settings
-from django.core.mail import mail_managers, EmailMultiAlternatives
+from django.core.mail import EmailMultiAlternatives
 from django.core.management.base import BaseCommand
 from django.template.loader import render_to_string
 from django_apscheduler import util
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
 
-from news.models import Post, Category, PostCategory
+from news.models import Post, Category
 
 logger = logging.getLogger(__name__)
 
